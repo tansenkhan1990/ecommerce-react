e65167290f778b55ba3f955a2478bf0f60c73183
tansenkhan1990 <tansenkhan1990@gmail.com>
2022-05-25 17:22:35 +0200

    add animation to the slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -31,6 +31,7 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
     height: 100%;
     display:flex;
+    transition: all 1.5s ease;
     transform: translateX(${props=>props.slideIndex * -100}vw);
 `
 const ImageContainer = styled.div`
