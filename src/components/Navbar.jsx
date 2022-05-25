4f08bc16ea9aebc8abfaa6de36b2d325fd86bdcb
tansenkhan1990 <tansenkhan1990@gmail.com>
2022-05-25 14:02:16 +0200

    add new design for navbar for logo and search button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { Search } from '@material-ui/icons'
 
 const Container = styled.div`
 height : 60px;
@@ -8,9 +9,12 @@ const Wrapper = styled.div`
 padding : 10px 20px;
 display : flex;
 justify-content : space-between;
+align-items: center;
 `
 const Left = styled.div`
 flex : 1;
+display : flex;
+align-items: center;
 `
 const Center = styled.div`
 flex : 1;
@@ -22,11 +26,28 @@ const Language = styled.span`
 font-size : 14px;
 cursor : pointer;
 `
+const SearchContainer = styled.div`
+border : .5px solid darkgray;
+display: flex;
+align-items: center;
+margin-left: 25px;
+padding: 5px;
+`
+const Input = styled.input`
+border: none;
+`
+const Logo = styled.h1`
+    text-align: center;
+    font-weight: bold;
+`
 const Navbar = () => {
   return (
     <Container><Wrapper>
-        <Left><Language>EN</Language></Left>
-        <Center>Center</Center>
+        <Left><Language>EN</Language>
+        <SearchContainer><Input />
+        <Search /></SearchContainer>
+        </Left>
+        <Center><Logo>My Business</Logo></Center>
         <Right>Right</Right>
         </Wrapper></Container>
   )
