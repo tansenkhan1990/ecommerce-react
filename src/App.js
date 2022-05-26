dec66d48dae25fa812f01b02cdb1f601c091a1db
tansenkhan1990 <tansenkhan1990@gmail.com>
2022-05-26 11:02:00 +0200

    add changes for the item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react'
 import Home from './pages/Home';
+
 function App() {
   return (
     <Home />
---
+++ src/responsive.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { css } from "styled-components";
+
+export const mobile = (props) => {
+  return css`
+    @media only screen and (max-width: 380px) {
+      ${props}
+    }
+  `;
+};
